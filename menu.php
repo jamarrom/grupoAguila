10ac5c7def6a8c07983d97ff863d8b645e961db8
Jamarrom <juanantonio.martinez2003@gmail.com>
2023-09-15 16:43:46 -0600

    inicio

---
+++ menu.php
@@ -0,0 +1,156 @@
+ 
+            <div class="left-nav">
+                <!-- start logo -->
+                <div class="sidebar-part1">
+                    <a href="index.php" title="Pofo" class="logo"><img src="img/icoArriba.png" data-at2x="img/icoArriba2x.png" alt="Pofo"></a> 
+                </div>
+                <!-- end logo -->
+                <div class="sidebar-part2">  
+                    <div class="sidebar-middle">
+                        <div class="sidebar-middle-menu alt-font font-weight-600">
+                            <nav class="navbar bootsnav navbar-expand-lg alt-font">  
+                                <div id="navbar-menu" class="collapse navbar-collapse no-padding">
+                                    <ul class="nav navbar-nav margin-80px-bottom">
+                                        <li class="dropdown">
+                                            <a href="index.php" title="INICIO" data-bs-toggle="dropdown">INICIO<!--<i class="fas fa-angle-right pull-right"></i>--></a>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-bs-toggle="dropdown" title="OBRA">OBRA <i class="fas fa-angle-right"></i></a>
+                                            <ul class="dropdown-menu second-level">
+                                                <li class="dropdown">
+                                                    <a href="pintura.php" title="Pintura" class="u-mayuscula">Pintura <i class="fas fa-angle-right"></i></a>
+                                                    <ul class="dropdown-menu third-level">
+                                                        <li><a href="pinturas-2021-2025.php">2021-2025</a></li>
+                                                        <li><a href="pinturas-2016-2020.php">2016-2020</a></li>
+                                                        <li><a href="pinturas-2011-2015.php">2011-2015</a></li>
+                                                        <li><a href="pinturas-2006-2010.php">2006-2010</a></li>
+                                                        <li><a href="pinturas-2000-2005.php">2000-2005</a></li>
+                                                        <li><a href="pinturas-1990s.php">1990s</a></li>
+                                                        <li><a href="pinturas-1980s.php">1980s</a></li>
+                                                        <li><a href="pinturas-1970s.php">1970s</a></li>
+                                                        <li><a href="pinturas-1960s.php">1960s</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="escultura.php" title="Escultura" class="u-mayuscula">Escultura <i class="fas fa-angle-right"></i></a>
+                                                    <ul class="dropdown-menu third-level">
+                                                        <li><a href="esculturas-2021-2025.php">2021-2025</a></li>
+                                                        <li><a href="esculturas-2016-2020.php">2016-2020</a></li>
+                                                        <li><a href="esculturas-2011-2015.php">2011-2015</a></li>
+                                                        <li><a href="esculturas-2006-2010.php">2006-2010</a></li>
+                                                        <li><a href="esculturas-2000-2005.php">2000-2005</a></li>
+                                                        <li><a href="esculturas-1990s.php">1990s</a></li>
+                                                        <li><a href="esculturas-1980s.php">1980s</a></li>
+                                                        <li><a href="esculturas-1970s.php">1970s</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="diseno.php" title="Diseno" class="u-mayuscula">Diseño <i class="fas fa-angle-right"></i></a>
+                                                    <ul class="dropdown-menu third-level">
+                                                        <li><a href="diseno-2014-vinos-rimbombantes.php">Vinos Rimbombantes</a></li>
+                                                        <li><a href="diseno-2015-sofa-mah-jong.php">Sofa Mah Jong</a></li>
+                                                        <li><a href="diseno-2015-TANE.php">Tane</a></li>
+                                                        <li><a href="diseno-2022-balones-wilson.php">Balones Wilson</a></li>
+                                                        <li><a href="diseno-el-aguijon.php">El aguijon</a></li>
+                                                        <li><a href="diseno-el-odabashian.php">Odabashian</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="#" title="Ediciones" class="u-mayuscula">Ediciones <i class="fas fa-angle-right"></i></a>
+                                                    <ul class="dropdown-menu third-level">
+                                                        <li>
+                                                            <a href="ediciones-graficas.php" title="Ediciones">Ediciones Gráficas</a>
+                                                        </li>
+                                                        <li><a href="ediciones-tridimencionales.php">Ediciones Tridimencionales</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-bs-toggle="dropdown" href="#" title="EXPOS">EXPOSICIONES <i class="fas fa-angle-right"></i></a>
+                                            <ul class="dropdown-menu second-level">
+                                                <li class="dropdown">
+                                                    <a href="#" title="Anteriores">Anteriores <i class="fas fa-angle-right"></i></a>
+                                                    <ul class="dropdown-menu third-level">
+                                                        <li><a href="casa-mexico-en-espana.php">Casa México en España</a></li>
+                                                        <li><a href="develacion-escultura.php">Develación Escultura</a></li>
+                                                        <li><a href="ethra.php">Ethra</a></li>
+                                                        <li><a href="extravagancias.php">Extravagancias</a></li>
+                                                        <li><a href="hipnerotomagia.php">Hipnerotomagia</a></li>
+                                                        <li><a href="la-casa-Irracional-franz-mayer-2016.php">La Casa Irracional Franz Mayer 2016</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="#" title="">Actuales</a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="#" title="">Futuras</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-bs-toggle="dropdown" href="" title="LIBROS">LIBROS  <i class="fas fa-angle-right"></i></a>
+                                            <ul class="dropdown-menu second-level">
+                                                <li class="dropdown">
+                                                    <a href="de-vacaciones-por-la-vida.php">De vacaciones por la vida</a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="emociones-arquitectonicas.php">Emociones Arquitectónicas</a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="la-casa-irracional.php">La casa irracional</a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="original-multiple.php">Original Múltiple</a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="pedro-frideberg-vida-y-obra.php">Pedro Frideberg - Vida y obra</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-bs-toggle="dropdown" href="#" title="BIOGRAFÍA">BIOGRAFÍA <i class="fas fa-angle-right"></i></a>
+                                            <ul class="dropdown-menu second-level">
+                                                <li class="dropdown">
+                                                    <a href="resumen-de-trayectoria.php" title="">Resumen de trayectoria </a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="cv/CV.pdf" title="" target=”_blank”>CV (el PDF)</a>
+                                                </li>
+                                                <li class="dropdown">
+                                                    <a href="cv-grafico.php" title="">CV gráfico</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-bs-toggle="dropdown" href="#" title="PROYECTOS">PROYECTOS<br />ESPECIALES <i class="fas fa-angle-right"></i></a>
+                                            <ul class="dropdown-menu second-level">
+                                                <li class="dropdown">
+                                                    <a href="melanoforis.php" title="">Melanoforis</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-bs-toggle="dropdown" href="autentificacion.php" title="ORIGINAL">AUTENTICIDAD</a>
+                                        </li>
+                                        <li>
+                                            <div class="side-left-menu-close close-side"></div>
+                                        </li>
+                                    </ul>
+                                </div>  
+                            </nav>
+                            <div class="right-bg"><img src="img/textoMenu.png" alt="Menu"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-part3">
+                    <div class="bottom-menu-icon">
+                        <a href="javascript:void(0);" class="right-menu-button text-extra-dark-gray nav-icon" id="showRightPush">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </a>
+                    </div> 
+                </div>
+            </div>
